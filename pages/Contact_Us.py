b2a4abff77f450182c0ce9e83c53b890176d2971
Zamer <aelizamer95@gmail.com>
2024-02-04 18:46:51 +0500

    Connected email sending script to contact us page

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,10 +1,18 @@
 import streamlit as sl
+from send_email import send_email
 
 sl.header("Contact Us")
 
 with sl.form(key="email_form"):
     user_email = sl.text_input("Your email:")
-    message = sl.text_area("Your Message:")
+    raw_message = sl.text_area("Your Message:")
+    message = f"""\
+    Subject: Python Portfolio Message
+    
+    From: {user_email}
+    {raw_message}
+"""
     button = sl.form_submit_button("Send")
     if button:
-        print("Hello")
+        send_email(message)
+        sl.info("Your email was sent!")
